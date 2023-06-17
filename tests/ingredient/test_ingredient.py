0cbe3aff3aa2f69797f64bf01083edbcf24cfbc4
Vitor Wagner <vitormwagner@gmail.com>
2023-06-17 15:54:10 -0300

    Feat: :sparkles: Added tests for `Ingredient` class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
+import pytest
 
 
-# Req 1a
+# Req 1
 def test_ingredient():
-    pass
+    cheese_1 = Ingredient("queijo gorgonzola")
+    cheese_2 = Ingredient("queijo gorgonzola")
+    ham_1 = Ingredient("presunto")
+
+    assert cheese_1.name == "queijo gorgonzola"
+    assert cheese_1.restrictions == {Restriction.LACTOSE,
+                                     Restriction.ANIMAL_DERIVED}
+    assert repr(cheese_1) == "Ingredient('queijo gorgonzola')"
+    assert hash(cheese_1) == hash("queijo gorgonzola")
+    assert hash(cheese_1) == hash(cheese_2)
+    assert hash(cheese_1) != hash(ham_1)
+    assert cheese_1 == cheese_2
+
+    with pytest.raises(TypeError):
+        Ingredient()
